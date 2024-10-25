911ecd4b22ac0348ff7a9ad92de72f2f7f60bf6c
Cento [NES] <cento@riudaura.org>
2024-10-25 10:22:56 +0200

    Create Apunts.py

---
+++ Apunts.py
@@ -0,0 +1,51 @@
+# Exemple us de variables
+#a = 12 
+#print(f"hola el numerro es {a} ")
+
+#[exercisi]
+
+#a = 11
+#b = 17
+
+#suma = a + b
+#resta = a - b
+#multi = a * b 
+#div = a / b 
+
+#print(f"La suma es {suma}")
+#print(f" La resta es {resta}")
+#print(f"La multi es {multi}")
+#print(f"La div es {div}")
+
+#-----------------------------------------------------------
+#Condicionals
+
+#Exemple 1
+
+#numero = 12 #Variable
+
+#if numero % 2 == 0: #el % es fa servir per obtenir del residu del num div 2 si el 
+# residu e 0 e num parell si no es senar
+
+#    print(f"{numero} es parell")
+
+#else:
+#    print(f"{numero} es senar")
+
+#[Exercisi]
+
+#numero = int(input("Introdueix un numero "))
+
+#if numero %2 == 0:
+    #print(f"el num es parell")
+#else:
+    #print(f"el num es imparell")
+
+#Exemple 2
+
+
+
+
+
+
+#-----------------------------------------------------------
