0b8754e48e6a3ebc39eb4940aaecd18ca1bab271
Cento [NES] <cento@riudaura.org>
2024-10-25 10:59:31 +0200

    update

--- Exercisis Condicionals.py
+++ Exercisis Condicionals.py
@@ -1,1 +1,24 @@
 #Aqui pordras observar exercisis de condicionals 
+
+
+
+#Exercici 1: Escriu un programa que demani un número i imprimeixi si 
+# és positiu, negatiu o zero.
+
+
+
+#Exercici 2: Escriu un programa que demani l'edat d'una persona i 
+#imprimeixi si és menor d'edat (menor de 18) o major d'edat.
+
+
+
+#Exercici 3: Escriu un programa que demani una temperatura i 
+# imprimeixi si és alta (per sobre de 30 graus), moderada 
+# (entre 15 i 30 graus) o baixa (per sota de 15 graus).
+
+
+
+#Exercici 4: Escriu un programa que demani a l'usuari una quantitat 
+# d'hores i calculi el cost total a pagar si la tarifa és de 10 
+# euros per hora. Si la quantitat d'hores és superior a 40, aplica 
+# una tarifa de 15 euros per hora per les hores extres.
--- Exercisis Variables.py
+++ Exercisis Variables.py
@@ -1,1 +1,23 @@
 #Aqui pordras observar exercisis de variables 
+
+#Exercici 1: Declara dues variables amb els teus números favorits i fes 
+# la suma, resta, multiplicació i divisió. Imprimeix els resultats de 
+# cadascuna d'aquestes operacions.
+
+
+
+#Exercici 2: Declara una variable amb el valor de l'edat d'una persona. 
+# Després, declara una segona variable amb el nombre d'anys fins a la 
+# seva jubilació (per exemple, 65 - edat) i imprimeix-la.
+
+
+
+#Exercici 3: Declara una variable amb la quantitat de productes i una 
+# altra amb el preu per producte. Calcula el total a pagar multiplicant
+#  aquests dos valors i imprimeix el resultat.
+
+
+
+#Exercici 4: Declara una variable amb el preu d’un objecte i una altra
+#  amb el percentatge d’impost (per exemple, 21%). Calcula el preu 
+# final aplicant aquest impost i imprimeix el resultat.
