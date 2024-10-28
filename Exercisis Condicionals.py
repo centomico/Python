023989c1f2d81ee90f1e7f9fbfdb884bc0496269
Cento [NES] <cento@riudaura.org>
2024-10-28 10:47:17 +0100

    Update Exercisis Condicionals.py

--- Exercisis Condicionals.py
+++ Exercisis Condicionals.py
@@ -5,23 +5,50 @@
 #Exercici 1: Escriu un programa que demani un número i imprimeixi si 
 # és positiu, negatiu o zero.
 
-numero = int(input(Posa un numero: ))
+#numero = int(input("Posa un numero: "))
 
-if 
-
+#if numero > 0:
+#    print("el num e posiitiu")
+#elif numero <= 0:
+#    print("el num es negatiu")
+#else:
+#    print ("el nuemro es 0")
 
 #Exercici 2: Escriu un programa que demani l'edat d'una persona i 
 #imprimeixi si és menor d'edat (menor de 18) o major d'edat.
 
+#edat = int(input("introdueix la teva edat: "))
 
+#if edat >= 18:
+#    print("Es major de edat")
+#else:
+#    print("es menor de edat")
 
 #Exercici 3: Escriu un programa que demani una temperatura i 
 # imprimeixi si és alta (per sobre de 30 graus), moderada 
 # (entre 15 i 30 graus) o baixa (per sota de 15 graus).
 
 
+temp = float(input("Introdueix la temperatura: "))
+
+
+if temp > 30:
+    print("La temperatura es alta.")
+elif temp >= 15:
+    print("La temperatura es moderada.")
+else:
+    print("La temperatura es baixa.")
 
 #Exercici 4: Escriu un programa que demani a l'usuari una quantitat 
 # d'hores i calculi el cost total a pagar si la tarifa és de 10 
 # euros per hora. Si la quantitat d'hores és superior a 40, aplica 
 # una tarifa de 15 euros per hora per les hores extres.
+
+h = int(input("Introdueix el nombre d'hores: "))
+
+if h > 40:
+    total = (40 * 10) + ((h - 40) * 15)
+else:
+    total = hores * 10
+
+print(f"El cost total a pagar és: {total} euros")
